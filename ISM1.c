05c14aac3dea8a8e0ad3227492f2a610fa22bc14
Kevin W. Palmer <kevinwpalmer@gmail.com>
2013-12-20 12:26:41 -0500

    Update ISM1.c

--- ISM1.c
+++ ISM1.c
@@ -73,25 +73,30 @@
 
 char const *_myvar;
 
-static const arg_t cont_args_def[] = {
+static const arg_t cont_args_def[] = 
+{
     POCKETSPHINX_OPTIONS,
     /* Argument file. */
     { "-argfile",
       ARG_STRING,
       NULL,
-      "Argument file giving extra arguments." },
+      "Argument file giving extra arguments." 
+	},
     { "-adcdev", 
       ARG_STRING, 
       NULL, 
-      "Name of audio device to use for input." },
+      "Name of audio device to use for input." 
+	},
     { "-infile", 
       ARG_STRING, 
       NULL, 
-      "Audio file to transcribe." },
+      "Audio file to transcribe." 
+	},
     { "-time", 
       ARG_BOOLEAN, 
       "no", 
-      "Print word times in file transcription." },
+      "Print word times in file transcription." 
+	},
     CMDLN_EMPTY_OPTION
 };
 
@@ -113,7 +118,8 @@ static void
 print_word_times(int32 start)
 {
 	ps_seg_t *iter = ps_seg_iter(ps, NULL);
-	while (iter != NULL) {
+	while (iter != NULL) 
+	{
 		int32 sf, ef, pprob;
 		float conf;
 		
@@ -124,91 +130,7 @@ print_word_times(int32 start)
 		iter = ps_seg_next (iter);
 	}
 }
-
-/*
- * Continuous recognition from a file
- */
-static void 
-recognize_from_file() 
-{
-    cont_ad_t *cont;
-    ad_rec_t file_ad = {0};
-    int16 adbuf[4096];
-    const char* hyp;
-    const char* uttid;
-    int32 k, ts, start;
-
-    char waveheader[44];
-    if ((rawfd = fopen(cmd_ln_str_r(config, "-infile"), "rb")) == NULL) {
-	E_FATAL_SYSTEM("Failed to open file '%s' for reading", 
-			cmd_ln_str_r(config, "-infile"));
-    }
-    
-    fread(waveheader, 1, 44, rawfd);
-
-    file_ad.sps = (int32)cmd_ln_float32_r(config, "-samprate");
-    file_ad.bps = sizeof(int16);
-
-    if ((cont = cont_ad_init(&file_ad, ad_file_read)) == NULL) {
-        E_FATAL("Failed to initialize voice activity detection");
-    }
-    if (cont_ad_calib(cont) < 0)
-        E_FATAL("Failed to calibrate voice activity detection\n");
-    rewind (rawfd);
-
-    for (;;) {
-
-	while ((k = cont_ad_read(cont, adbuf, 4096)) == 0);
-	
-        if (k < 0) {
-    	    break;
-    	}
-
-        if (ps_start_utt(ps, NULL) < 0)
-            E_FATAL("ps_start_utt() failed\n");
-
-        ps_process_raw(ps, adbuf, k, FALSE, FALSE);
-        
-        ts = cont->read_ts;
-        start = ((ts - k) * 100.0) / file_ad.sps;
-        
-        for (;;) {
-            if ((k = cont_ad_read(cont, adbuf, 4096)) < 0)
-            	break;
-
-            if (k == 0) {
-                /*
-                 * No speech data available; check current timestamp with most recent
-                 * speech to see if more than 1 sec elapsed.  If so, end of utterance.
-                 */
-                if ((cont->read_ts - ts) > DEFAULT_SAMPLES_PER_SEC)
-                    break;
-            }
-            else {
-                /* New speech data received; note current timestamp */
-                ts = cont->read_ts;
-            }
-
-
-            ps_process_raw(ps, adbuf, k, FALSE, FALSE);
-        }
-
-        ps_end_utt(ps);
-        
-        if (cmd_ln_boolean_r(config, "-time")) {
-	    print_word_times(start);
-	} else {
-	    hyp = ps_get_hyp(ps, NULL, &uttid);
-            printf("%s: %s\n", uttid, hyp);
-        }
-        fflush(stdout);	
-    }
-
-    cont_ad_close(cont);
-    fclose(rawfd);
-}
-
-/* Sleep for specified msec */
+* Sleep for specified msec */
 static void
 sleep_msec(int32 ms)
 {
@@ -233,7 +155,8 @@ sleep_msec(int32 ms)
  * 	   print utterance result;
  *     }
  */
-static void recognize_from_microphone()
+static void 
+recognize_from_microphone()
 {
     ad_rec_t *ad;
     int16 adbuf[4096];
@@ -244,19 +167,26 @@ static void recognize_from_microphone()
     cont_ad_t *cont;
     char word[256];
 
-    if ((ad = ad_open_dev(cmd_ln_str_r(config, "-adcdev"),
-                          (int)cmd_ln_float32_r(config, "-samprate"))) == NULL)
-        E_FATAL("Failed to open audio device\n");
+    if (
+			(
+				ad = ad_open_dev
+					(
+					cmd_ln_str_r(config, "-adcdev"),(int)cmd_ln_float32_r(config, "-samprate")
+					)
+			) == NULL
+		)E_FATAL("Failed to open audio device\n");
 
     /* Initialize continuous listening module */
-    if ((cont = cont_ad_init(ad, ad_read)) == NULL)
-        E_FATAL("Failed to initialize voice activity detection\n");
+    if (
+			(cont = cont_ad_init(ad, ad_read)) == NULL
+		) E_FATAL("Failed to initialize voice activity detection\n");
     if (ad_start_rec(ad) < 0)
         E_FATAL("Failed to start recording\n");
     if (cont_ad_calib(cont) < 0)
         E_FATAL("Failed to calibrate voice activity detection\n");
 
-    for (;;) {
+    for (;;) 
+	{
         /* Indicate listening for next utterance */
         printf("READY....\n");
         fflush(stdout);
@@ -283,11 +213,13 @@ static void recognize_from_microphone()
         ts = cont->read_ts;
 
         /* Decode utterance until end (marked by a "long" silence, >1sec) */
-        for (;;) {
+        for (;;) 
+		{
             /* Read non-silence audio data, if any, from continuous listening module */
             if ((k = cont_ad_read(cont, adbuf, 4096)) < 0)
                 E_FATAL("Failed to read audio\n");
-            if (k == 0) {
+            if (k == 0) 
+			{
                 /*
                  * No speech data available; check current timestamp with most recent
                  * speech to see if more than 1 sec elapsed.  If so, end of utterance.
@@ -295,7 +227,8 @@ static void recognize_from_microphone()
                 if ((cont->read_ts - ts) > DEFAULT_SAMPLES_PER_SEC)
                     break;
             }
-            else {
+            else 
+			{
                 /* New speech data received; note current timestamp */
                 ts = cont->read_ts;
             }
@@ -323,7 +256,7 @@ static void recognize_from_microphone()
         /* Finish decoding, obtain and print result */
         ps_end_utt(ps);
         hyp = ps_get_hyp(ps, NULL, &uttid);
-	_myvar=hyp;
+		_myvar=hyp;
         printf("%s: %s: %s\n", uttid, hyp, _myvar);
 
 	
@@ -331,7 +264,8 @@ static void recognize_from_microphone()
         fflush(stdout);
 
         /* Exit if the first word spoken was GOODBYE */
-        if (hyp) {
+        if (hyp) 
+		{
             sscanf(hyp, "%s", word);
             if (strcmp(word, "goodbye") == 0)
                 break;
@@ -395,28 +329,3 @@ int main(int argc, char *argv[])
     ps_free(ps);
     return 0;
 }
-
-/** Silvio Moioli: Windows CE/Mobile entry point added. */
-#if defined(_WIN32_WCE)
-#pragma comment(linker,"/entry:mainWCRTStartup")
-#include <windows.h>
-
-//Windows Mobile has the Unicode main only
-int wmain(int32 argc, wchar_t *wargv[]) {
-    char** argv;
-    size_t wlen;
-    size_t len;
-    int i;
-
-    argv = malloc(argc*sizeof(char*));
-    for (i=0; i<argc; i++){
-        wlen = lstrlenW(wargv[i]);
-        len = wcstombs(NULL, wargv[i], wlen);
-        argv[i] = malloc(len+1);
-        wcstombs(argv[i], wargv[i], wlen);
-    }
-
-    //assuming ASCII parameters
-    return main(argc, argv);
-}
-#endif
